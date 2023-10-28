d4e0e369fbb203345f707dcee7360baa64e588ff
peshkes <peshkesv@gmail.com>
2023-10-28 19:48:47 +0300

    Created architecture, added short links and plugin for module-css

---
+++ src/modules/main-page/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './MainPage.module.css';
+
+const MainPage = () => {
+    return (
+        <div className={style.mainPage}>
+            poook
+        </div>
+    );
+};
+
+export default MainPage;
