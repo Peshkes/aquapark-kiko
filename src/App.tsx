378d9c2d3dbbda2b331e3b216fdd8c687d9dc100
peshkes <peshkesv@gmail.com>
2024-02-02 21:26:52 +0200

    Added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import MainPage from 'modules/main-page/MainPage';
 import React, {useEffect} from 'react';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import StorePage from "modules/store-page/StorePage";
+import DocumentsPage from "modules/documents-page/DocumentsPage";
 
 function App() {
     useEffect(()=>{
@@ -10,7 +13,11 @@ function App() {
     },[])
     return (
         <div className="App">
-            <MainPage/>
+            <Routes>
+                <Route path={"/"} element={<MainPage/>}/>
+                <Route path={"/store"} element={<StorePage/>}/>
+                <Route path={"/documents"} element={<DocumentsPage/>}/>
+            </Routes>
         </div>
     );
 }
